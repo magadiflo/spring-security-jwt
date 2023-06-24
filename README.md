2896adbbf98e36f70a5732b8232e3ab1bea754dc
Martín <magadiflo@gmail.com>
2023-06-23 23:59:34 -0500

    Implementando el método de registro

--- README.md
+++ README.md
@@ -985,3 +985,34 @@ public class AuthenticationService {
     }
 }
 ````
+
+## [01:45:37] Implement the register method
+
+Implementamos la lógica de registro en la clase de servicio AuthenticateService:
+
+````java
+
+@RequiredArgsConstructor
+@Service
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(this.passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        this.userRepository.save(user);
+        String jwtToken = this.jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    /* omitted code */
+}
+````
--- src/main/java/com/magadiflo/app/auth/AuthenticationService.java
+++ src/main/java/com/magadiflo/app/auth/AuthenticationService.java
@@ -1,14 +1,32 @@
 package com.magadiflo.app.auth;
 
+import com.magadiflo.app.config.JwtService;
+import com.magadiflo.app.repositories.UserRepository;
+import com.magadiflo.app.user.Role;
+import com.magadiflo.app.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(this.passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        this.userRepository.save(user);
+        String jwtToken = this.jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
